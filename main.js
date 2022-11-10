bf714d993e9fd3161179b965495489fdabb71929
Nicolò Casagrande <nicolo.casagrande7@gmail.com>
2022-11-10 07:52:30 +0100

    impostazione file

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const images = [
     {
         image: 'img/01.webp',
@@ -21,3 +23,61 @@ const images = [
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
+
+// seleziono il contenitore delle mie immagini
+const slider = document.getElementById('image-container');
+
+// creo l'aray in cui dovro inserire la stringa con gli src delle immagini
+images.forEach(element => {
+    const divCreation = document.createElement(`div`);
+    divCreation.classList.add('slide');
+    divCreation.innerHTML = `<img src="${element.image}" alt="">`;
+    slider.append(divCreation);
+});
+
+// seleziono il primo div e gli attribuisco la classe active
+document.querySelector('.slide').classList.add('active');
+
+let active = 0;
+
+const up = document.getElementById('arrow-up');
+const down = document.getElementById('arrow-down');
+
+// creo un array contenente tutti i div con classe slide
+var elements = document.querySelectorAll('.slide');
+
+// con questa funzione, al click della freccia up rimuovo la classe active all'elemento attivo, creo un ciclo if per azzerare il contatore se siamo sull'ultima foto e poi applico la classe active al successivo elemento
+up.addEventListener('click', nextSlide);
+
+// con questa funzione, al click della freccia up rimuovo la classe active all'elemento attivo, creo un ciclo if per azzerare il contatore se siamo sull'ultima foto e poi applico la classe active al successivo elemento
+down.addEventListener('click', prevSlide);
+
+// let autoplay = setInterval(nextSlide, 3000);
+
+function nextSlide(){
+    elements[active].classList.remove('active');
+    if(active===img.length - 1){
+        active = 0;
+    }else{
+        active++;
+    }
+    elements[active].classList.add('active');
+}
+
+function prevSlide(){
+    elements[active].classList.remove('active');
+    if(active===0){
+        active = img.length - 1;
+    }else{
+        active--;
+    }
+    elements[active].classList.add('active');
+}
+
+slider.addEventListener('mouseenter', function(){
+    clearInterval(autoplay);
+});
+
+slider.addEventListener('mouseleave', function(){
+    autoplay = setInterval(nextSlide, 3000);
+});
